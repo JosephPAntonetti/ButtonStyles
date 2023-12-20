868c3b7fd83ee677ebb456f2a547f1c35707791c
Joe Antonetti <antwwwon@icloud.com>
2023-12-20 15:56:32 -0500

    Added ToolbarButtonStyle

---
+++ Sources/ButtonStyles/Toolbar/ToolbarButtonStyle.swift
@@ -0,0 +1,49 @@
+//
+//  ToolbarButtonStyle.swift
+//
+//
+//  Created by Joseph Antonetti on 12/20/23.
+//
+
+import SwiftUI
+
+public struct ToolbarButtonStyle : ButtonStyle {
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(.body, design: .rounded, weight: .semibold))
+            .symbolRenderingMode(.hierarchical)
+            .foregroundStyle(.primary)
+            .padding(.all, 4)
+            .background {
+                Circle()
+                    .foregroundStyle(.primary.quinary)
+            }
+            .overlay {
+                if configuration.isPressed {
+                    Circle()
+                        .foregroundStyle(.black)
+                        .opacity(0.3)
+                        .shadow(radius: configuration.isPressed ? 4 : 0)
+                }
+            }
+    }
+}
+
+#Preview {
+    NavigationView {
+        Text("Content")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("", systemImage: "plus") {}
+                        .buttonStyle(ToolbarButtonStyle())
+                        .foregroundStyle(.blue)
+                }
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("", systemImage: "highlighter") {}
+                        .buttonStyle(ToolbarButtonStyle())
+                        .foregroundStyle(.green)
+                }
+            }
+    }
+}
